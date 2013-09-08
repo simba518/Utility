644175a4d100fac2dd3929ccd93e81ce1ee278fb
= <=>
2013-09-08 21:26:43 +0800

    begin to use boost.test.

---
+++ TestCase/CommonTest/test.cpp
@@ -0,0 +1,13 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(FancyShmancyLogic)
+
+BOOST_AUTO_TEST_CASE(TestingIf2x3equals6)
+{
+}
+
+BOOST_AUTO_TEST_CASE(TestingIf2x2equals4)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ TestCase/CommonTest/test2.cpp
@@ -0,0 +1,13 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(FancyShmancyLogic)
+
+BOOST_AUTO_TEST_CASE(TestingIf2x3equals600)
+{
+}
+
+BOOST_AUTO_TEST_CASE(TestingIf2x2equals433)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
