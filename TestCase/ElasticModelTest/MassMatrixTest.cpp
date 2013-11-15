d1bc6102c11ddcf09181c603b317cd242db7d546
= <=>
2013-11-15 15:39:45 +0800

    implement the un-lumped mass matrix, test passed.

--- TestCase/ElasticModelTest/MassMatrixTest.cpp
+++ TestCase/ElasticModelTest/MassMatrixTest.cpp
@@ -21,17 +21,29 @@ BOOST_AUTO_TEST_CASE(TestCompMassMat){
   SparseMatrix<double> ref_M;
   TEST_ASSERT ( load(ref_M,mass_mat_f) );
   ASSERT_EQ(ref_M.rows(),n*3);
-  DiagonalMatrix<double,-1> lumped_ref_M;
-  MassMatrix::lump(ref_M,lumped_ref_M);
-  
+
+  // test lumped matrix
   MassMatrix mass;
   DiagonalMatrix<double,-1> M;
   mass.compute(M,mesh);
+
+  DiagonalMatrix<double,-1> lumped_ref_M;
+  MassMatrix::lump(ref_M,lumped_ref_M);
 
   ASSERT_EQ(M.rows(),n*3);
   ASSERT_EQ(M.rows(),lumped_ref_M.rows());
   const MatrixXd diffM = lumped_ref_M.diagonal()-(M.diagonal());
   ASSERT_LE(diffM.norm(),1e-10);
+
+  // test unlumped matrix
+  SparseMatrix<double> fullM;
+  mass.compute(fullM,mesh);
+  ASSERT_EQ(fullM.rows(),n*3);
+  ASSERT_EQ(fullM.cols(),n*3);
+  SparseMatrix<double> fullM_t = fullM.transpose();
+  ASSERT_LE((fullM_t-fullM).norm(),1e-10);
+  ASSERT_LE((fullM-ref_M).norm(),1e-10);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
