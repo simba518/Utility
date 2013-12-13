a93d2fdbb53f65b5c805dabcd2cea34477473883
= <=>
2013-12-13 16:42:02 +0800

    extend the assertext to support printing more information.

--- SRC/Common/assertext.h
+++ SRC/Common/assertext.h
@@ -20,12 +20,20 @@
 # define assert_lt(value_a,value_b)		
 # define assert_in(value_a,min,max)
 
+# define assert_eq_ext(value_a,value_b,info)		
+# define assert_ne_ext(value_a,value_b,info)		
+# define assert_ge_ext(value_a,value_b,info)		
+# define assert_gt_ext(value_a,value_b,info)		
+# define assert_le_ext(value_a,value_b,info)		
+# define assert_lt_ext(value_a,value_b,info)		
+# define assert_in_ext(value_a,min,max,info)
+
 #else
 
 // define a new assert micro to throw expections instead of abort
+#define my_assert( e, file, line ) ( throw std::runtime_error(std::string(file)+":"+boost::lexical_cast<std::string>(line)+": failed assertion "+e))
+
 #define new_assert(e) ((void) ((e) ? 0 : my_assert (#e, __FILE__, __LINE__)))
-#define my_assert( e, file, line ) ( throw std::runtime_error(\
-   std::string(file)+":"+boost::lexical_cast<std::string>(line)+": failed assertion "+e))
 
 // assert "value_a == value_b", if not, print the values and abort.
 #define assert_eq(value_a, value_b)										\
@@ -33,8 +41,18 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a == value_b);										\
-	}														
+	  new_assert(value_a == value_b);									\
+	}																	\
+  
+#define assert_eq_ext(value_a, value_b, info)							\
+  if (value_a != value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a == value_b);									\
+	}
+
 
 // assert "value_a != value_b", if not, print the values and abort.
 #define assert_ne(value_a, value_b)										\
@@ -42,7 +60,16 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a != value_b);										\
+	  new_assert(value_a != value_b);									\
+	}														
+
+#define assert_ne_ext(value_a, value_b, info)							\
+  if (value_a == value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a != value_b);									\
 	}														
 
 // assert "value_a >= value_b", if not, print the values and abort.
@@ -51,7 +78,16 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a >= value_b);										\
+	  new_assert(value_a >= value_b);									\
+	}														
+
+#define assert_ge_ext(value_a, value_b, info)							\
+  if (value_a < value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a >= value_b);									\
 	}														
 
 // assert "value_a > value_b", if not, print the values and abort.
@@ -60,7 +96,16 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a > value_b);										\
+	  new_assert(value_a > value_b);									\
+	}														
+
+#define assert_gt_ext(value_a, value_b, info)							\
+  if (value_a <= value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a > value_b);									\
 	}														
 
 // assert "value_a >= value_b", if not, print the values and abort.
@@ -69,7 +114,16 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a <= value_b);										\
+	  new_assert(value_a <= value_b);									\
+	}														
+
+#define assert_le_ext(value_a, value_b, info)							\
+  if (value_a > value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a <= value_b);									\
 	}														
 
 // assert "value_a > value_b", if not, print the values and abort.
@@ -78,18 +132,37 @@
 	{																	\
 	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
 	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
-	  new_assert(value_a < value_b);										\
+	  new_assert(value_a < value_b);									\
+	}														
+
+#define assert_lt_ext(value_a, value_b, info)							\
+  if (value_a >= value_b)												\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(value_b) <<" = " << value_b<< std::endl;	\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a < value_b);									\
 	}														
 
 // assert "value_a in [min,max]", if not, print the values and abort.
 #define assert_in(value_a, min, max)									\
-if ( !(value_a >= min && value_a <= max) )								\
-  {																		\
-	std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;		\
-	std::cout << __STRING(min) <<" = " << min << std::endl;				\
-	std::cout << __STRING(max) <<" = " << max << std::endl;				\
-	new_assert(value_a >= min && value_a <= max);							\
-  }														
+  if ( !(value_a >= min && value_a <= max) )							\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(min) <<" = " << min << std::endl;			\
+	  std::cout << __STRING(max) <<" = " << max << std::endl;			\
+	  new_assert(value_a >= min && value_a <= max);						\
+	}														
+
+#define assert_in_ext(value_a, min, max, info)							\
+  if ( !(value_a >= min && value_a <= max) )							\
+	{																	\
+	  std::cout << __STRING(value_a) <<" = " << value_a<< std::endl;	\
+	  std::cout << __STRING(min) <<" = " << min << std::endl;			\
+	  std::cout << __STRING(max) <<" = " << max << std::endl;			\
+	  std::cout << "additional info: "<< info << std::endl;				\
+	  new_assert(value_a >= min && value_a <= max);						\
+	}														
 
 #endif /* NDEBUG.  */
 
